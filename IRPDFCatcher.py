72ebcba16dc21f894e6aa5aaf75fa27b0e93f3d1
D'Aguila <34548216+daguila93@users.noreply.github.com>
2023-01-02 18:53:29 -0300

    Update IRPDFCatcher.py

--- IRPDFCatcher.py
+++ IRPDFCatcher.py
@@ -4,6 +4,8 @@ from pathlib import Path
 import pandas as pd
 import slate3k as slate
 
+currentPath = Path(r'C:\desenv\Python\IR PDF Catcher\files') #Caminho para a pasta com os pdfs de I.R
+
 
 def extract_data(file):
     rows = list()
@@ -15,14 +17,14 @@ def extract_data(file):
 
             for row in rows_regex.finditer(document_text):
                 matched_group = f'{date} {row.group(1)}'
-                replaced = re.sub('\s(PN|N[0-9]|CI|#|ON|ER|EJ|NM|EX)', '', matched_group)
+                replaced = re.sub('\s(PN|N[0-9]|CI|#|ON|ER|EJ|NM|EX|ED|EDJ)', '', matched_group)
                 splitted = replaced.split(' ')
                 rows.append(splitted)
 
     return rows
 
 
-currentPath = Path('./files')
+
 date_regex = re.compile('\d{2}\/\d{2}\/\d{4}')
 rows_regex = re.compile('BOVESPA\s(.*?)\s[C,D]\s')
 data = list()
@@ -30,7 +32,7 @@ data = list()
 for child in currentPath.iterdir():
     data += extract_data(child)
 
-dt = pd.DataFrame(data, columns=['Data Pregão', 'C/V', 'Tipo de Mercado','Especificação do Título', 'Quantidade', 'Preço/Ajuste', 'Valor/Ajuste'])
+dt = pd.DataFrame(data, columns=['Data Pregão', 'C/V', 'Tipo de Mercado','Especificação do Título',  'Quantidade', 'Preço/Ajuste', 'Valor/Ajuste'])
 
 dt.to_csv('ir.csv', sep=';', encoding='utf-8', index=False)
 dt.to_excel('ir.xlsx', index=False)
