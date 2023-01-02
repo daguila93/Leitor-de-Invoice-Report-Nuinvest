3ee0f7f3a551872788a8d3d96cecceddd6a77770
D'Aguila <34548216+daguila93@users.noreply.github.com>
2023-01-02 19:17:50 -0300

    Update IRPDFCatcher.py

--- IRPDFCatcher.py
+++ IRPDFCatcher.py
@@ -4,7 +4,7 @@ from pathlib import Path
 import pandas as pd
 import slate3k as slate
 
-currentPath = Path(r'C:\desenv\Python\IR PDF Catcher\files') #Caminho para a pasta com os pdfs de I.R
+currentPath = Path(r'C:\desenv\Python\IR PDF Catcher\files') #Coloque o caminho dos arquivos pdfs dos invoices aqui
 
 
 def extract_data(file):
@@ -14,10 +14,12 @@ def extract_data(file):
         with open(file, 'rb') as f:
             document_text = slate.PDF(f).text()
             date = date_regex.findall(document_text)[2]
+            invoice = invoice_number.findall(document_text)[0]
+            # print(invoice)
 
             for row in rows_regex.finditer(document_text):
-                matched_group = f'{date} {row.group(1)}'
-                replaced = re.sub('\s(PN|N[0-9]|CI|#|ON|ER|EJ|NM|EX|ED|EDJ)', '', matched_group)
+                matched_group = f'{invoice} {date} {row.group(1)}'
+                replaced = re.sub('\s(PN|N[0-9]|CI|#|ON|ER|EJ|NM|EX|EDJ|ED)', '', matched_group)
                 splitted = replaced.split(' ')
                 rows.append(splitted)
 
@@ -26,13 +28,14 @@ def extract_data(file):
 
 
 date_regex = re.compile('\d{2}\/\d{2}\/\d{4}')
+invoice_number = re.compile('nota (.{0,}) Data')
 rows_regex = re.compile('BOVESPA\s(.*?)\s[C,D]\s')
 data = list()
 
 for child in currentPath.iterdir():
     data += extract_data(child)
 
-dt = pd.DataFrame(data, columns=['Data Pregão', 'C/V', 'Tipo de Mercado','Especificação do Título',  'Quantidade', 'Preço/Ajuste', 'Valor/Ajuste'])
+dt = pd.DataFrame(data, columns=['Nota de Negociação', 'Data Pregão', 'C/V', 'Tipo de Mercado','Especificação do Título',  'Quantidade', 'Preço/Ajuste', 'Valor/Ajuste'])
 
 dt.to_csv('ir.csv', sep=';', encoding='utf-8', index=False)
 dt.to_excel('ir.xlsx', index=False)
